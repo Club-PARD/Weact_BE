47f456ef9627efe1e7310f99a329e291432918a7
binjiseong <9900bjs@gmail.com>
2025-06-30 18:06:36 +0900

    first

---
+++ src/main/java/com/pard/weact/WeactApplication.java
@@ -0,0 +1,13 @@
+package com.pard.weact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeactApplication.class, args);
+    }
+
+}
