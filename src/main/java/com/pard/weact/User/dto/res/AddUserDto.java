705e2f0b273178a8949f93712a90c3789684ed99
chianddo223 <whddjs0210@naver.com>
2025-07-05 17:42:47 +0900

    fix yaml

---
+++ src/main/java/com/pard/weact/User/dto/res/AddUserDto.java
@@ -0,0 +1,10 @@
+package com.pard.weact.User.dto.res;
+
+import lombok.*;
+
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter @Builder
+public class AddUserDto {
+    private String userId;
+}
+
