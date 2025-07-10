aa28a5d88b96cb7044ba84eaddb48e82dbbbf1f2
binjiseong <9900bjs@gmail.com>
2025-07-11 05:56:55 +0900

    plus post log

--- src/main/java/com/pard/weact/habitPost/service/HabitPostService.java
+++ src/main/java/com/pard/weact/habitPost/service/HabitPostService.java
@@ -25,6 +25,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.time.LocalDate;
